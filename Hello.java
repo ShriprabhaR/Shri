db31aef9f0b0a19325e0208824e682f0b8c29000
ShriprabhaR <121273908+ShriprabhaR@users.noreply.github.com>
2023-11-21 19:54:46 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package com.shri.dayoneee;
+
+public class Hello {
+
+
+		public static void main(String[] args) {
+			
+            System.out.println("I");
+            System.out.println("Love");
+            System.out.println("java");
+           
+            System.out.println("programming");
+            System.out.println("Language");
+            System.out.println("Thank u");
+        
+		}
+
+}
